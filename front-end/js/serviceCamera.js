9729187f948db7635dd3bcf17d8c79ddebf933fd
MarineFlora <marine.rapeneau@gmail.com>
2021-08-25 15:26:56 +0200

    ajout fonction js loadCameras et selectCamera

--- front-end/js/serviceCamera.js
+++ front-end/js/serviceCamera.js
@@ -1,6 +1,7 @@
 /* méthode fetch pour récupérer la liste des cameras du serveur et les afficher sur la page d'accueil index.html */ //
 
-fetch("http://localhost:3000/api/cameras/")
+function loadCameras() {
+    fetch("http://localhost:3000/api/cameras/")
     .then(data => data.json()) // transforme données reçues en format json
     .then(jsonListCamera => { // affiche les données dans la console
         for(let jsonCamera of jsonListCamera) { // boucle qui parcours le tableau, pour chaque case du tableau elle créé une variable jsonCamera qui pourra être manipulée directement
@@ -10,10 +11,15 @@ fetch("http://localhost:3000/api/cameras/")
                                                                 <img class="card-img-top" src="${camera.imageUrl}" alt="camera vintage" />
                                                                 <div class="card-body text-center">
                                                                     <h2 class="card-title">${camera.name}</h2>
-                                                                    <p class="card-text price fw-bold">${camera.price/100} €</p>
-                                                                    <a href="#" class="btn btn-primary">Voir produit</a>
+                                                                    <p class="card-text price fw-bold">${camera.price} €</p>
+                                                                    <a href="#" onClick="selectCamera('${camera.id}');" class="btn btn-primary">Voir produit</a>
                                                                 </div>
                                                             </div>
                                                           </div>` //  permet l'affichage : pour l'élement ID 'cards', on va remplacer dans le template les élements voulus avec l'objet camera. Interpolation de caractère : ${variable voulue}
         }
     });
+}
+
+function selectCamera(cameraId) {
+    alert("Vous avez sélectionné la caméra Id= " + cameraId); 
+}
