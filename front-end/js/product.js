d22f78c184e79cdc946cd5b192b441c4425b4374
MarineFlora <marine.rapeneau@gmail.com>
2021-08-30 18:58:37 +0200

    refactor fonction displayPopUp dans fonction addToCart

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -57,29 +57,30 @@ function addToCart() {
         localStorage.setItem("cart", JSON.stringify(cart)); // conversion en JSON
         let cartRestored = JSON.parse(localStorage.getItem("cart")); //reconversion de l'objet en JS
         console.log(cartRestored); //variable cartRestored contient maintenant l'objet qui avait été sauvegardé dans le localStorage
-        displayPopUp()
         
-    }
-
-}
-//Affichage message pour confirmer ajout panier + résumé + rediriger vers panier ou accueil
-function displayPopUp() {
-    document.getElementById("pop-up").innerHTML += `<div class="modal" id="exampleModalCenter" tabindex="-1" role="dialog" aria-hidden="true">
+        //Affichage message pour confirmer ajout panier + résumé + rediriger vers panier ou accueil
+        document.getElementById("pop-up").innerHTML += `<div class="modal" id="exampleModalCenter" tabindex="-1" role="dialog" aria-hidden="true">
                                                         <div class="modal-dialog modal-dialog-centered" role="document">
                                                             <div class="modal-content">
                                                                 <div class="modal-header">
-                                                                    <h5 class="modal-title text-success"> 
+                                                                    <h3 class="modal-title text-success"> 
                                                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
                                                                         <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
                                                                         </svg>
                                                                         Ajouté au panier !
-                                                                  </h5> 
+                                                                    </h3> 
                                                                 </div>
-                                                                <div class="modal-body d-flex flex-row justify-content-between align-items-center">
+                                                                <div class="modal-body d-flex flex-row justify-content-around align-items-center">
                                                                     <img src="${camera.imageUrl}" alt="camera vintage ${camera.name}" class="camera-mini"/>
-                                                                    <p>${camera.name}</p>
-                                                                   
-                                                                    <p class="fw-bold">${camera.price/100} €</p>
+                                                                    <div>
+                                                                        <p class="fw-bold">Modèle</p>
+                                                                        <p>${camera.name}</p>
+                                                                    </div>
+                                                                    <div>
+                                                                        <p class="fw-bold">Lentille</p>
+                                                                        <p>${selectedLens}</p>
+                                                                    </div>
+                                                
                                                                 </div>
                                                                 <div class="modal-footer">
                                                                     <a href="index.html" class="btn border-0">< Continuer mes achats</a>
@@ -88,5 +89,8 @@ function displayPopUp() {
                                                             </div>
                                                         </div>
                                                     </div>`
-  }
-  
+        
+    }
+
+}
+
