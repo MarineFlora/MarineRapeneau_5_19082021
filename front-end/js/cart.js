910c7b953ad51fa33413883b78f0fbce64032adc
MarineFlora <marine.rapeneau@gmail.com>
2021-09-07 15:28:29 +0200

    ajout prix total

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -1,6 +1,6 @@
 //-----------------------------------récupération du panier-----------------------------------//
 let cartRestored = JSON.parse(localStorage.getItem("cart"));
-
+let totalPrice =0;
 // fonction pour afficher le contenu de la page panier et récuperer les elements selectionnés
 function loadCart() { 
     if (cartRestored == null) {
@@ -23,6 +23,7 @@ function loadCart() {
                                                                     <h1 class="pb-3 col"> Mon panier (${cartRestored.length} produits)</h1>`;
         // boucle pour chaque élement enregistré dans le local storage, ajouter cet html
         cartRestored.forEach(item => {
+            totalPrice+=Number(item.price);
             document.getElementById("cart-products").innerHTML += ` <div class=" col-sm-3 mb-2">
                                                                         <img id="product-img" class="camera-mini" src="${item.imageUrl}" alt="camera vintage ${item.name} " />
                                                                     </div>
@@ -47,7 +48,7 @@ function loadCart() {
         document.getElementById("cart-total").innerHTML += `<h2 class="row text-left pb-2">Récapitulatif</h2>
                                                                 <div class="row bg-light p-3 justify-content-center rounded">
                                                                     <p class="col fw-bold text-center">TOTAL</p>
-                                                                    <p class="col fw-bold text-center">333 €</p>
+                                                                    <p class="col fw-bold text-center">${totalPrice} €</p>
                                                                     <p class="row fw-light fst-italic">Hors frais de livraison</p>
                                                                     <a href="#" class="btn btn-primary my-4 w-auto">Valider mon panier</a>
                                                                 </div>`;
