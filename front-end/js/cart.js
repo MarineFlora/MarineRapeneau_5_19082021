b2cd4917c811fff95d6662ce5cab3b1499f4abd7
MarineFlora <marine.rapeneau@gmail.com>
2021-09-16 12:30:50 +0200

    fix pattern formulaire

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -29,7 +29,7 @@ function loadCart() {
                                                                         </div>
                                                                         <p class="col-sm-2 fw-bold mb-2 price-product" >${item.price} €</p>  
                                                                         <div class="col-md-2 mb-2 d-flex justify-content-center align-items-center">
-                                                                            <input type="number" min="1" max="100" value="${item.quantity}"  id="${item._id}" class="form-control form-select-sm input-sm input-vh" onblur="priceUpdate(event, '${item._id}')" onchange="priceUpdate(event, '${item._id}')">
+                                                                            <input type="number" min="1" max="100" value="${item.quantity}"  id="${item._id}" class="form-control form-select-sm input-sm input-vh" onblur="priceUpdate(event, '${item._id}')" onchange="priceUpdate(event, '${item._id}')" />
                                                                         </div>
                                                                         <a href="cart.html" class="col-md-2 mb-4" onclick="removeItem('${item._id}')">supprimer</a> `;
             });
